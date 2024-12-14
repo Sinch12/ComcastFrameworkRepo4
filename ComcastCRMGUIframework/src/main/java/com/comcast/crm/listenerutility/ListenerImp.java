8fa4232195085e89f6f541001a9065571a72d0bb
Sinchana K S <Sinchana K S@LAPTOP-8SEKENFG>
2024-12-14 13:09:19 +0530

    commit1

--- ComcastCRMGUIframework/src/main/java/com/comcast/crm/listenerutility/ListenerImp.java
+++ ComcastCRMGUIframework/src/main/java/com/comcast/crm/listenerutility/ListenerImp.java
@@ -84,25 +84,21 @@ report.setSystemInfo("BROWSER", "CHROME-100");
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
 		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
 	}
 
 	@Override
 	public void onTestFailedWithTimeout(ITestResult result) {
-		// TODO Auto-generated method stub
 		ITestListener.super.onTestFailedWithTimeout(result);
 	}
 
 	@Override
 	public void onStart(ITestContext context) {    
-		// TODO Auto-generated method stub
 		ITestListener.super.onStart(context);
 	}
 
 	@Override
 	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
 		ITestListener.super.onFinish(context);
 	}
 	
